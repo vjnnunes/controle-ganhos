aa74710b4ccac200c23dba743144af18cded04f7
vjnnunes <vjnnunes@gmail.com>
2026-05-05 19:47:19 -0300

    Update service-worker.js
    Updated service worker to use a new cache version and added activation logic to delete old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,28 @@
+const CACHE_NAME = "controle-ganhos-v2";
+
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open("controle-ganhos-v1").then(cache => {
-      return cache.addAll(["./index.html", "./manifest.json"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
     })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
